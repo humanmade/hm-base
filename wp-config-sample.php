678991113621b58380de34df9ffacd1e9faafd7f
Tom Willmot <tom@humanmade.co.uk>
2013-02-15 10:06:10 +0000

    No need to hardcode the site_url and home_url

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -5,7 +5,7 @@ define( 'DB_USER', 'database_username_here' );
 define( 'DB_PASSWORD', 'database_password_here' );
 define( 'DB_HOST', 'database_host_here' );
 
-define( 'WP_SITEURL', 'http://localhost/hm-base/wordpress' );
-define( 'WP_HOME', 'http://localhost/hm-base' );
+define( 'WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/wordpress' );
+define( 'WP_HOME', 'http://' . $_SERVER['HTTP_HOST'] );
 
 define( 'HM_DEV', true ); // Enable/disable HM Dev Pluign. Also Enables/disables wp_debug
