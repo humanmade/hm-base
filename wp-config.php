e11ea80cd1e1435f28d1afb906c93a5eec7da249
Tom Willmot <tom@humanmade.co.uk>
2013-02-15 10:03:55 +0000

    Code formatting

--- wp-config.php
+++ wp-config.php
@@ -117,7 +117,7 @@ define( 'WPMU_PLUGIN_URL', WP_HOME . '/content/plugins-mu' );
 define( 'WP_DEFAULT_THEME', 'twentytwelve' );
 
 // Prevent editing of files through the admin.
-define('DISALLOW_FILE_EDIT', true);
+define( 'DISALLOW_FILE_EDIT', true );
 
 /* That's all, stop editing! Happy blogging. */
 
