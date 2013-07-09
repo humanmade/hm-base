9859d541f5e0efc0fd2855cf1750e61543183f1b
Matth_eu <matthew@matth.eu>
2013-07-09 12:13:54 +0100

    use correct asset paths

--- www/content/themes/hm-base-theme/index.php
+++ www/content/themes/hm-base-theme/index.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <title>Testing Grunt</title>
 	
-	<link href="<?php echo get_template_directory_uri(); ?>/assets/css/style.min.css" media="all" rel="stylesheet" type="text/css" />
-    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/test.min.js" type="text/javascript"></script>
+	<link href="<?php echo get_template_directory_uri(); ?>/assets/css/hm-base.min.css" media="all" rel="stylesheet" type="text/css" />
+    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/hm-base.min.js" type="text/javascript"></script>
     
 </head>
 
