2f638fe0a329b724cc00da05d60698bfcbb89712
Matth_eu <matthew@matth.eu>
2013-10-24 16:38:17 +0100

    uncomment

--- content/plugins-mu/hm-base-environment.php
+++ content/plugins-mu/hm-base-environment.php
@@ -9,23 +9,13 @@ add_filter( 'got_rewrite', '__return_true', 999 );
  * If you go to /wp-admin/ when wordpress is in a subdirectory, it cleverly redirects you to /subdir/wp-login.ph
  * But... it appends reauth=1 to the url... which forces a reauthentication on a succesful login... wierd right!
  */
-// add_filter( 'login_url', function( $url ) {
+add_filter( 'login_url', function( $url ) {
 
-// 	if ( '/wp-admin/' === add_query_arg( array() ) ) {
-// 		$url = remove_query_arg( 'reauth', $url );
-// 		$url = add_query_arg( 'redirect_to', get_admin_url(), $url );
-// 	}
+	if ( '/wp-admin/' === add_query_arg( array() ) ) {
+		$url = remove_query_arg( 'reauth', $url );
+		$url = add_query_arg( 'redirect_to', get_admin_url(), $url );
+	}
 	
-// 	return $url;
+	return $url;
 
-// } );
-
-
-
-// add_action( 'wp_head', function() {
-
-// 	global $wp_rewrite;
-
-// 	hm( $wp_rewrite );
-
-// } );
+} );
