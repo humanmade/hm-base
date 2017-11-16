c12f1b252fda916f8d4c2db5bf5667881f7464ae
Nathaniel Schweinberg <natanielks@gmail.com>
2017-11-15 18:27:01 -0800

    wrap redis/memcached servers in defineds to prevent error notices

--- wp-config-production-sample.php
+++ wp-config-production-sample.php
@@ -3,22 +3,27 @@
 $table_prefix  = getenv('TABLE_PREFIX') ?: 'wp_';
 
 foreach ($_ENV as $key => $value) {
-    $capitalized = strtoupper($key);
-    if (!defined($capitalized)) {
-        define($capitalized, $value);
-    }
+	$capitalized = strtoupper($key);
+	if (!defined($capitalized)) {
+		define($capitalized, $value);
+	}
 }
-global $memcached_servers;
-$memcached_servers = array( array( MEMCACHED_HOST, MEMCACHED_PORT ) );
 
-global $redis_server;
-$redis_server = array(
-	'host' => REDIS_HOST,
-	'port' => REDIS_PORT,
-);
+if ( defined('MEMCACHED_HOST') && defined('MEMCACHED_PORT') ) {
+	global $memcached_servers;
+	$memcached_servers = array( array( MEMCACHED_HOST, MEMCACHED_PORT ) );
+}
+
+if ( defined('REDIS_HOST') && defined('REDIS_PORT') ) {
+	global $redis_server;
+	$redis_server = array(
+		'host' => REDIS_HOST,
+		'port' => REDIS_PORT,
+	);
+}
 
 
 if( defined('DISPLAY_ERRORS') && DISPLAY_ERRORS ) {
-    error_reporting(E_ALL);
-    @ini_set('display_errors', 1);
+	error_reporting(E_ALL);
+	@ini_set('display_errors', 1);
 }
