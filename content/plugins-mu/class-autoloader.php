bb01e56170eb9c1dc768d1251600de1db634e885
Ryan McCue <me@ryanmccue.info>
2018-07-12 16:54:28 +1000

    Check that autoloader hasn't already been declared

--- content/plugins-mu/class-autoloader.php
+++ content/plugins-mu/class-autoloader.php
@@ -2,47 +2,52 @@
 
 namespace HM\Autoloader;
 
-class Autoloader {
-	const NS_SEPARATOR = '\\';
+// Skip declaration if loaded via hm-platform
+if ( ! class_exists( 'HM\\Autoloader\\Autoloader' ) ) {
+	class Autoloader {
+		const NS_SEPARATOR = '\\';
 
-	protected $prefix;
-	protected $prefix_length;
-	protected $path;
+		protected $prefix;
+		protected $prefix_length;
+		protected $path;
 
-	public function __construct( $prefix, $path ) {
-		$this->prefix        = $prefix;
-		$this->prefix_length = strlen( $prefix );
-		$this->path          = trailingslashit( $path );
-	}
-
-	public function load( $class ) {
-		if ( strpos( $class, $this->prefix . self::NS_SEPARATOR ) !== 0 ) {
-			return;
+		public function __construct( $prefix, $path ) {
+			$this->prefix        = $prefix;
+			$this->prefix_length = strlen( $prefix );
+			$this->path          = trailingslashit( $path );
 		}
 
-		// Strip prefix from the start (ala PSR-4)
-		$class = substr( $class, $this->prefix_length + 1 );
-		$class = strtolower( $class );
-		$file  = '';
+		public function load( $class ) {
+			if ( strpos( $class, $this->prefix . self::NS_SEPARATOR ) !== 0 ) {
+				return;
+			}
 
-		// Split on namespace separator.
-		$last_ns_pos = strripos( $class, self::NS_SEPARATOR );
-		if ( $last_ns_pos !== false ) {
-			$namespace = substr( $class, 0, $last_ns_pos );
-			$class     = substr( $class, $last_ns_pos + 1 );
-			$file      = str_replace( self::NS_SEPARATOR, DIRECTORY_SEPARATOR, $namespace ) . DIRECTORY_SEPARATOR;
-		}
-		$file .= 'class-' . str_replace( '_', '-', $class ) . '.php';
+			// Strip prefix from the start (ala PSR-4)
+			$class = substr( $class, $this->prefix_length + 1 );
+			$class = strtolower( $class );
+			$file  = '';
 
-		$path = $this->path . $file;
+			// Split on namespace separator.
+			$last_ns_pos = strripos( $class, self::NS_SEPARATOR );
+			if ( $last_ns_pos !== false ) {
+				$namespace = substr( $class, 0, $last_ns_pos );
+				$class     = substr( $class, $last_ns_pos + 1 );
+				$file      = str_replace( self::NS_SEPARATOR, DIRECTORY_SEPARATOR, $namespace ) . DIRECTORY_SEPARATOR;
+			}
+			$file .= 'class-' . str_replace( '_', '-', $class ) . '.php';
 
-		if ( file_exists( $path ) ) {
-			require_once $path;
+			$path = $this->path . $file;
+
+			if ( file_exists( $path ) ) {
+				require_once $path;
+			}
 		}
 	}
 }
 
-function register_class_path( $prefix, $path ) {
-	$loader = new Autoloader( $prefix, $path );
-	spl_autoload_register( [ $loader, 'load' ] );
+if ( ! function_exists( 'HM\\Autoloader\\register_class_path' ) ) {
+	function register_class_path( $prefix, $path ) {
+		$loader = new Autoloader( $prefix, $path );
+		spl_autoload_register( [ $loader, 'load' ] );
+	}
 }
