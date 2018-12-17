9148048bb3888d7d4f9553023ecf627744ea127f
Mike Selander <mikeselander@gmail.com>
2018-12-17 11:45:34 -0700

    Add filter for a unique uploads directory

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -38,5 +38,25 @@ tests_add_filter( 'muplugins_loaded', function() {
 	remove_action( 'wp_version_check', 'wp_version_check' );
 } );
 
+/**
+ * Re-map the default `/uploads` folder with our own `/test-uploads` for tests.
+ *
+ * WordPress core runs a method (scan_user_uploads) on the first instance of
+ * `WP_UnitTestCase`. This method scans every single folder and file in the
+ * uploads directory.
+ *
+ * This filter prevents any potential issues arising from running imports
+ * locally and speeds up overall test execution. We do this by adding a unique
+ * test uploads folder just for our tests to reduce load.
+ */
+tests_add_filter( 'upload_dir', function( $dir ) {
+	array_walk( $dir, function( &$item ) {
+		if ( is_string( $item ) ) {
+			$item = str_replace( '/uploads', '/test-uploads', $item );
+		}
+	} );
+	return $dir;
+}, 20 ); // Ensure this fires late, after other filtering has occurred.
+
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
